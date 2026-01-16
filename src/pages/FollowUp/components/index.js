07acb133ff0fca3691cc553ca9c89b20bf2cba30
Abdur Rahman Khan <abdurrahman.khan@pelsoftlabs.in>
2026-01-16 14:46:11 +0530

    fixed

--- src/pages/FollowUp/components/index.js
+++ src/pages/FollowUp/components/index.js
@@ -1,5 +1,5 @@
 export { default as StatCard } from './StatCard';
 export { default as FollowupCard } from './FollowupCard';
-export { default as FollowupHeader } from './FollowupHeader';
+export { default as FollowupHeader } from './FollowUpHeader';
 export { default as FollowupToolbar } from './FollowupToolbar';
 export { default as FollowupPagination } from './FollowupPagination';
